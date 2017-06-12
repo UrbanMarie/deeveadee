2b647716eb7c9d3def69da81c45c72ff34ee3288
marie-urbano <hippchz@gmail.com>
2017-06-12 16:42:45 +0000

    rajout page dvd

--- application/views/dvd.php
+++ application/views/dvd.php
@@ -11,9 +11,48 @@
 	  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+	<link href="<? echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  </head>
+  <body>
 
+<div class="container" style="margin-top:20px;">
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+          <ul class="nav navbar-nav navbar-right">
+              <li><a href="<?php echo site_url('admin'); ?>"class="list-group-item"> ADMIN</a></li>
+              <li><a href="<?php echo site_url('dvd'); ?>"class="list-group-item"> Films</a></li>
+            <li><a href="">Bonjour</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="<?php echo site_url('home/logout') ?>">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
 <div id="container">
-<h1> DEEVEADEE </h1>
+<h4> Liste des films </h4>
 
 <!-- Old -->
 <!--
@@ -50,17 +89,17 @@
 
 
 <!-- View films -->
-<!--	<div class="row">
+	<div class="row">
 	  <div class="col-sm-3 col-md-4">
 	    <div class="thumbnail">
-		<?php// foreach($articles as $article): ?>
-	      <a><img src="<?php //echo $article['JaquetteD'] ;?>" width="20%"></a>
+		<?php foreach($articles as $article): ?>
+	      <a><img src="<?php echo $article['JaquetteD'] ;?>" width="20%"></a>
 	      <div class="caption">
-			<h3><?php// echo $article['titreD']; ?></h3>
-			<p><?php// echo $article['auteurD']; ?></p>
-			<p><?php// echo $article['anneeD']; ?></p>
-	        <p><a href="<?php// echo site_url('admin'); ?>" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-			<?php// endforeach ?>
+			<h3><?php echo $article['titreD']; ?></h3>
+			<p><?php echo $article['auteurD']; ?></p>
+			<p><?php echo $article['anneeD']; ?></p>
+	        <p><a href="<?php echo site_url('admin'); ?>" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			<?php endforeach ?>
 		 </div>
 
 	    </div>
@@ -87,8 +126,8 @@
 		<div class='row'>
 	  </div>
 	</nav>
-</div>-->
-<!-- fin view films -->
+</div>
+
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
